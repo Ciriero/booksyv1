08201ef2f6bc02a1cd918e3ab856fcc9bac57293
Christian Calle C <chcallec@gmail.com>
2024-07-22 19:52:12 +0200

    added first route and check it works

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>HOME</h1>
+  },
+  {
+    path: "/about",
+    element: <h5>About</h5>
+  }
+])
 
 const App = () => {
-  return (
-    <h1>Booksy</h1>
-  )
+  return <RouterProvider router={router}/>
 }
 
 export default App
