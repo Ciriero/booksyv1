4b79c83c51d4017c6bfbbfa555231de6c3a99a97
Christian Calle C <chcallec@gmail.com>
2024-07-23 12:16:51 +0200

    added errorElement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,6 +19,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLy />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import {Link, useRouteError} from "react-router-dom";
 
 const Error = () => {
+  const error = useRouteError()
   return (
+    <div>
     <h1>Error</h1>
+    <Link to="/">Go Home</Link>
+     </div>
   )
 }
 
