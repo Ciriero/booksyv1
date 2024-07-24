2ecce45f8c51e8a75a514e536aababbf70c7d93a
Christian Calle C <chcallec@gmail.com>
2024-07-24 10:58:42 +0200

    added error svg and error.jsx

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import {Link, useRouteError} from "react-router-dom";
+import { WrapperError } from '../styles/Error.styles';
+import error from "../assets/error.svg"
 
 const Error = () => {
-  const error = useRouteError()
+  const error = useRouteError();
+
+  if(error.status === 404){
+    return <WrapperError>
+      <img src={error} alt="404 error" />
+      <h3>Sorry!!! Page not found</h3>
+      <Link to="/" className='btn'>Come bach home</Link>
+    </WrapperError>
+  }
+
+
   return (
     <div>
     <h1>Error</h1>
---
+++ frontend/src/styles/Error.styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const WrapperError = styled.div`
+
+`
